795ff7cf8b3695be521414a63207aabb9ee4f9d9
Ziky <ziky_zhiqi_jin@163.com>
2025-02-03 10:18:45 +0800

    init client & serve

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+// import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import react from '@vitejs/plugin-react';
+import path from 'path'; // 如果编辑器提示 path 模块找不到，则可以安装一下 @types/node -> pnpm i @types/node -D
 
-// https://vite.dev/config/
+function resolve(dir: string) {
+  return path.join(__dirname, '.', dir);
+}
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // 配置别名
+  resolve: {
+    alias: {
+      '@': resolve('src'), // 设置 `@` 指向 `src` 目录
+    },
+  },
+});
